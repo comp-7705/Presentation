6ccab00f527710ac6c67694fa6b732ac64b1a5fb
lykdsb <1697353364@qq.com>
2023-06-05 11:19:20 +0800

    added some images,adjusted code styles

--- src/slides/June8.vue
+++ src/slides/June8.vue
@@ -2,7 +2,8 @@
 import { loadReveal, Cover, HSection, VSection, Image } from "@js/slide.js";
 import imgHMER from "@images/June8/hmer.jpg";
 import imgTradition from "@images/June8/tradition.png";
-
+import imgOCR from "@images/June8/ocr.jpg";
+import imgTedious from "@images/June8/tedious.png"
 loadReveal();
 </script>
 
@@ -20,7 +21,7 @@ loadReveal();
                             extracting mathematical expressions from handwritten
                             input.
                         </p>
-                        <Image :src="imgHMER" alt="hmer" class="w-1/2" />
+                        <Image :src="imgHMER" alt="hmer" class="w-2/5" />
                     </VSection>
                     <VSection>
                         <h2>Definition of HMER</h2>
@@ -29,12 +30,12 @@ loadReveal();
                             converted into LaTeX format, which facilitates
                             computer processing.
                         </p>
-                        <code>
+                        <code style="font-size:x-large;">
                             \begin{align} y &= (x+1)^2 \\ &= x^2+2x+1
                             \end{align}
                         </code>
                         <p class="text-center">↓</p>
-                        <pre style="width: 40%">
+                        <pre style="font-size:xx-large; width: 60%;">
                             <code data-trim>
                                 \begin{align}
                                 y &= (x+1)^2 \\
@@ -94,6 +95,11 @@ loadReveal();
                                 </ul>
                             </li>
                         </ol>
+                        <Image
+                            :src="imgOCR"
+                            alt="OCR"
+                            class="w-3/5"
+                        />
                     </VSection>
                 </section>
                 <section>
@@ -110,26 +116,26 @@ loadReveal();
                             * Editing formulas on the computer was a challenging task,thus save time for researchers
                             * Automatically grade handwritten mathematical assignments
                             * Interactive online courses that allow students to enter handwritten formulas and receive immediate feedback
+                            <image class="w-3/5" src="/src/assets/images/June8/tedious.png" style="margin:0 auto;"/>
                         </textarea>
                     </section>
                     <section data-markdown>
-                        <textarea data-template>
+                        <textarea data-template >
                             ## Industry and Business
-                            * Digitize hand-drawn diagrams and engineering sketches
-                            * Digitize handwritten financial documents in accounting
-                        </textarea>
-                    </section>
-                    <section data-markdown>
-                        <textarea data-template>
-                            ## Overall Advantages
-                            1. Increased Efficiency and Productivity
-                            2. Improved Accuracy and Precision
-                            3. Enhanced Accessibility and Usability
+                            * In engineering, handwritten formula recognition can be used to digitize hand-drawn diagrams and engineering sketches, making them easier to edit and share.
+                            * n finance, handwritten formula recognition can be used to digitize handwritten financial documents, such as invoices and receipts. This can help to streamline accounting processes, reduce errors, and improve efficiency.
+                            <image class="w-2/5" src="/src/assets/images/June8/digitization.png" style="margin:0 auto;"/>
                         </textarea>
                     </section>
                 </section>
                 <section>
-                    <section>III. Principal</section>
+                    <section>
+                        <div class="center">
+                            <div>
+                                <h1>III. Methodology</h1>
+                            </div>
+                        </div>
+                    </section>
                 </section>
                 <HSection text="Thank you!" />
             </div>
