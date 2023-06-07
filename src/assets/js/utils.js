fa406196ce6b4559f51650d09d3ffbc0a9281968
Link Chen <linkch@qq.com>
2023-06-07 21:11:40 +0800

    Fix path issue

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,12 +1,16 @@
 export function gallery(imagePaths) {
     const images = {}; // Access the images by their name
-    // console.log(imagePaths);
+    console.log(imagePaths);
     for (const path of imagePaths) {
         // Extract the image name from the URL
         let name = path.split("/").pop();
-        name = name.split(".")[0]; // Remove the extension
+        name = name.substring(0, name.lastIndexOf(".")); // Remove the extension
+        // if in build mode, remove the hash value
+        if (import.meta.env.PROD) {
+            name = name.substring(0, name.lastIndexOf("-"));
+        }
         images[name] = path;
     }
-    // console.log(images);
+    console.log(images);
     return images;
 }
