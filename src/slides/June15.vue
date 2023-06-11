245ff8285fda239fe0785bd18452587a4801a6f1
Link Chen <linkch@qq.com>
2023-06-11 21:44:56 +0800

    Update June15

--- src/slides/June15.vue
+++ src/slides/June15.vue
@@ -1,6 +1,15 @@
 <script setup>
-import { loadReveal } from "@js/slide.js";
-const n = 3;
+import { loadReveal, Cover, HSection, VSection, Image } from "@js/slide.js";
+import { gallery } from "@js/utils.js";
+// Import all images from the Home folder, only relative paths here, issue of vite
+const imagePaths = Object.values(
+    import.meta.glob("../assets/images/June8/*.{png,jpg,jpeg,PNG,JPEG,svg}", {
+        eager: true,
+        as: "url",
+    })
+);
+const im = gallery(imagePaths);
+
 loadReveal();
 </script>
 
@@ -8,12 +17,26 @@ loadReveal();
     <div class="w-screen h-screen">
         <div class="reveal">
             <div class="slides">
-                <section>June 15 Title</section>
-                <section v-for="i in n" :key="i">
-                    <section>Section{{ i }}</section>
-                    <section v-for="j in n" :key="j">{{ i }}.{{ j }}</section>
+                <Cover date="June 15" />
+                <section>
+                    <HSection text="I. Failure Case" />
+                    <VSection>
+                        <h2>Title</h2>
+                    </VSection>
                 </section>
-                <section>End</section>
+                <section>
+                    <HSection text="II. Website Demo" />
+                    <VSection>
+                        <h2>Title</h2>
+                    </VSection>
+                </section>
+                <section>
+                    <HSection text="III. Schedule" />
+                    <VSection>
+                        <h2>Title</h2>
+                    </VSection>
+                </section>
+                <HSection text="Thank you!" />
             </div>
         </div>
     </div>
